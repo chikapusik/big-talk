d4ca7df6c311620b4d43b53faebe267eb0743a86
Natalia Perinova <natashaperinova@gmail.com>
2025-12-02 18:35:32 +0400

    Added animation in the background decorated the buttons

--- script.js
+++ script.js
@@ -32,16 +32,42 @@ function goBack() {
 
     const lastCard = cards[cards.length - 1];
 
-    lastCard.classList.add("flip-out");
+    lastCard.classList.add("flip-back-out");
 
     setTimeout(() => {
         deck.prepend(lastCard);
 
-        lastCard.classList.remove("flip-out");
-        lastCard.classList.add("flip-in");
+        lastCard.classList.remove("flip-back-out");
+        lastCard.classList.add("flip-back-in");
 
         setTimeout(() => {
-            lastCard.classList.remove("flip-in");
+            lastCard.classList.remove("flip-back-in");
         }, 300);
     }, 300);
+}
+
+const snowContainer = document.querySelector('.snow');
+const snowflakeCount = 80; 
+
+for(let i = 0; i < snowflakeCount; i++){
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    snowflake.textContent = '❄'; 
+
+    snowflake.style.left = Math.random() * 100 + 'vw';
+
+
+    snowflake.style.fontSize = 10 + Math.random() * 20 + 'px';
+
+  
+    snowflake.style.animationDuration = 4 + Math.random() * 6 + 's';
+
+
+    snowflake.style.animationDelay = Math.random() * 5 + 's';
+
+
+    snowflake.style.setProperty('--x-move', (Math.random() * 100 - 50) + 'px');
+    snowflake.style.setProperty('--rotate', (Math.random() * 360) + 'deg');
+
+    snowContainer.appendChild(snowflake);
 }
